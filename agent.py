b08eef87bc4486777d2c5774c5fe51c4b3c1752f
pkorobov <korobov.i.am@gmail.com>
2020-01-29 14:48:43 +0300

    logging weights and eval

--- agent.py
+++ agent.py
@@ -1,9 +1,9 @@
 from recsim.agent import AbstractEpisodicRecommenderAgent
-
-from environment import *
+import tensorflow as tf
 from wolpertinger.wolp_agent import *
-
 from stable_baselines.ddpg import MlpPolicy
+from gym import spaces
+import os
 
 class StaticAgent(AbstractEpisodicRecommenderAgent):
 
@@ -17,17 +17,25 @@ class StaticAgent(AbstractEpisodicRecommenderAgent):
 
 class WolpAgent(AbstractEpisodicRecommenderAgent):
 
-    def __init__(self, env, action_space, k_ratio=0.1, max_actions=1000):
+    def __init__(self, env, action_space, k_ratio=0.1, policy_kwargs=None,
+                 action_noise=None, eval_mode=False, max_actions=1000):
         AbstractEpisodicRecommenderAgent.__init__(self, action_space)
 
         self._observation_space = env.observation_space
-        self.agent = WolpertingerAgent(MlpPolicy, env, k_ratio=k_ratio)
-
+        self.agent = WolpertingerAgent(MlpPolicy, env, tau=1e-5, action_noise=action_noise,
+                                       policy_kwargs=policy_kwargs, k_ratio=k_ratio)
         self.t = 0
         self.current_episode = {}
+        self.eval_mode = eval_mode
+
+        max_tf_checkpoints_to_keep = 10
+        with self.agent.get_sess().as_default(), self.agent.get_graph().as_default():
+            # print("#################################")
+            # print(tf.all_variables())
+            self._saver = tf.train.Saver(var_list=tf.all_variables(), max_to_keep=max_tf_checkpoints_to_keep)
 
     def begin_episode(self, observation=None):
-        self.agent._setup_learn(0)
+        self.agent._reset()
         state = self._extract_state(observation)
         return self._act(state)
 
@@ -58,12 +66,56 @@ class WolpAgent(AbstractEpisodicRecommenderAgent):
             "reward": reward,
             "done": done
         })
-
-        self.agent._store_transition(**self.current_episode)
-        if self.agent.replay_buffer.can_sample(self.agent.batch_size):
-            critic_loss, actor_loss = self.agent._train_step(self.t, None)#, log=t_train == 0)
+        if not self.eval_mode:
+            self.agent._store_transition(**self.current_episode)
+            if self.agent.replay_buffer.can_sample(self.agent.batch_size):
+                self.agent._train_step(self.t, None)
+                self.agent._update_target_net()
         self.current_episode = {}
 
     def _extract_state(self, observation):
         user_space = self._observation_space.spaces['user']
         return spaces.flatten(user_space, observation['user'])
+
+    def bundle_and_checkpoint(self, checkpoint_dir, iteration_number):
+
+        if not tf.gfile.Exists(checkpoint_dir):
+            return None
+        # Call the Tensorflow saver to checkpoint the graph.
+
+        self._saver.save(
+            self.agent.get_sess(),
+            os.path.join(checkpoint_dir, 'tf_ckpt'),
+            global_step=iteration_number)
+
+        # Checkpoint the out-of-graph replay buffer.
+        # self._replay.save(checkpoint_dir, iteration_number)
+        bundle_dictionary = {}
+        bundle_dictionary['episode_num'] = self._episode_num
+
+        # bundle_dictionary['state'] = self.state
+        # bundle_dictionary['training_steps'] = self.training_steps
+        return bundle_dictionary
+
+    def unbundle(self, checkpoint_dir, iteration_number, bundle_dictionary):
+        # try:
+        #     self._replay.load(checkpoint_dir, iteration_number)
+        # except tf.errors.NotFoundError:
+        #     if not self.allow_partial_reload:
+        #         # If we don't allow partial reloads, we will return False.
+        #         return False
+        #       tf.logging.warning('Unable to reload replay buffer!')
+        # if bundle_dictionary is not None:
+        #     for key in self.__dict__:
+        #         if key in bundle_dictionary:
+        #             self.__dict__[key] = bundle_dictionary[key]
+        # elif not self.allow_partial_reload:
+        #     return False
+        # else:
+        #     tf.logging.warning("Unable to reload the agent's parameters!")
+        # Restore the agent's TensorFlow graph.
+
+        self._saver.restore(self.agent.get_sess(),
+                            os.path.join(checkpoint_dir,
+                                         'tf_ckpt-{}'.format(iteration_number)))
+        return True
--- experiment.py
+++ experiment.py
@@ -4,9 +4,15 @@ import shutil
 from recsim.agents import full_slate_q_agent
 from recsim.agents.random_agent import RandomAgent
 from recsim.simulator import recsim_gym, environment, runner_lib
+from stable_baselines.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise, AdaptiveParamNoiseSpec
+import tensorflow as tf
 
-from agent import *
+from environment import *
+from agent import WolpAgent, StaticAgent
 
+RUNS = 1
+MAX_TRAINING_STEPS = 15
+NUM_ITERATIONS = 400
 
 def create_random_agent(sess, environment, eval_mode, summary_writer=None):
     return RandomAgent(environment.action_space, random_seed=SEED)
@@ -30,10 +36,13 @@ def create_dqn_agent(sess, environment, eval_mode, summary_writer=None):
     return full_slate_q_agent.FullSlateQAgent(sess, **kwargs)
 
 
-def create_wolp_agent_with_ratio(k_ratio=0.1):
+def create_wolp_agent_with_ratio(k_ratio=0.1, policy_kwargs=None, action_noise=None):
 
     def create_wolp_agent(sess, environment, eval_mode, summary_writer=None):
-        return WolpAgent(environment, action_space=environment.action_space, k_ratio=k_ratio)
+        # action_noise_ = None if eval_mode else action_noise
+        return WolpAgent(environment, action_space=environment.action_space,
+                         k_ratio=k_ratio, policy_kwargs=policy_kwargs,
+                         action_noise=action_noise, eval_mode=eval_mode)
 
     return create_wolp_agent
 
@@ -46,40 +55,103 @@ def cleanup_dir(dir_path):
     return dir_path
 
 
+class EvalRunnerCustom(runner_lib.EvalRunner):
+    def __init__(self, max_eval_episodes = 125000,
+                 test_mode = False,
+                 min_interval_secs = 30,
+                 train_base_dir = None,
+                 **kwargs):
+        super(EvalRunnerCustom, self).__init__(max_eval_episodes = max_eval_episodes,
+                                               test_mode = test_mode,
+                                               min_interval_secs = min_interval_secs,
+                                               train_base_dir = train_base_dir,
+                                               **kwargs)
+
+    def _run_eval_phase(self, total_steps):
+        """Runs evaluation phase given model has been trained for total_steps."""
+
+        self._env.reset_sampler()
+        self._initialize_metrics()
+
+        num_episodes = 0
+        episode_rewards = []
+
+        while num_episodes < self._max_eval_episodes:
+            _, episode_reward = self._run_one_episode()
+            episode_rewards.append(episode_reward)
+            num_episodes += 1
+            self._write_metrics(num_episodes, suffix='eval')
+
+        output_file = os.path.join(self._output_dir, 'returns_%s' % total_steps)
+        tf.compat.v1.logging.info('eval_file: %s', output_file)
+        with tf.io.gfile.GFile(output_file, 'w+') as f:
+            f.write(str(episode_rewards))
+
+
 def main():
     """
     See results with to compare different agents
       tensorboard --logdir /tmp/recsim
     """
-    base_dir = cleanup_dir('/tmp/recsim/')
-
     env = recsim_gym.RecSimGymEnv(
         environment.Environment(UserModel(), DocumentSampler(), DOC_NUM, 1, resample_documents=False),
         clicked_reward
     )
 
+    policy_kwargs = {'layers': [64, 64]}
+    # noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(DOC_NUM), sigma=0.3 * np.ones(DOC_NUM))
+    noise = NormalActionNoise(mean=np.zeros(DOC_NUM), sigma=0.2 * np.ones(DOC_NUM))
+
+    num_actions = lambda actions, k_ratio: max(1, round(actions * k_ratio))
+
     agents = [
                # ("random", create_random_agent),
-               # ("good", create_good_agent),
+               ("optimal", create_good_agent),
                # ("bad", create_bad_agent),
-               ('wolpertinger_0.1', create_wolp_agent_with_ratio(0.1)),
-               ('wolpertinger_0.25', create_wolp_agent_with_ratio(0.25)),
-               ('wolpertinger_0.5', create_wolp_agent_with_ratio(0.5)),
-               ('wolpertinger_1.0', create_wolp_agent_with_ratio(1)),
-               ("DQN", create_dqn_agent),
+               # ('wolpertinger_1_neighbour', create_wolp_agent_with_ratio(0, action_noise=OrnsteinUhlenbeckActionNoise)),
+               ('wolpertinger_0.01_' + "(" + str(num_actions(DOC_NUM, 0.01)) + ")",
+                                                                  create_wolp_agent_with_ratio(0.01,
+                                                                  action_noise=noise,
+                                                                  policy_kwargs=policy_kwargs)),
+               # ('wolpertinger_0.05_' + "(" + str(num_actions(DOC_NUM, 0.05)) + ")",
+               #                                                    create_wolp_agent_with_ratio(0.05,
+               #                                                    action_noise=noise,
+               #                                                    policy_kwargs=policy_kwargs)),
+               # ('wolpertinger_0.1_' + "(" + str(num_actions(DOC_NUM, 0.1)) + ")",
+               #                                                   create_wolp_agent_with_ratio(0.1,
+               #                                                   action_noise=noise,
+               #                                                   policy_kwargs=policy_kwargs)),
+               ('wolpertinger_1.0_' + "(" + str(num_actions(DOC_NUM, 1.0)) + ")",
+                                                                 create_wolp_agent_with_ratio(1,
+                                                                 action_noise=noise,
+                                                                 policy_kwargs=policy_kwargs))
     ]
+
+    base_dir = cleanup_dir('logs/')
     for agent_name, create_agent_fun in agents:
-        env.reset()
+        for run in range(RUNS):
+            dir = base_dir + agent_name + "/run_" + str(run)
+            env.reset()
 
-        runner = runner_lib.TrainRunner(
-            base_dir=base_dir + agent_name,
-            create_agent_fn=create_agent_fun,
-            env=env,
-            max_training_steps=500,
-            num_iterations=100
-        )
-        runner.run_experiment()
+            runner = runner_lib.TrainRunner(
+                base_dir=dir,
+                create_agent_fn=create_agent_fun,
+                env=env,
+                max_training_steps=MAX_TRAINING_STEPS,
+                num_iterations=NUM_ITERATIONS
+            )
+            runner.run_experiment()
 
+            runner = EvalRunnerCustom(
+                base_dir=dir,
+                create_agent_fn=create_agent_fun,
+                env=env,
+                max_eval_episodes=1000,
+                test_mode=True
+            )
+            runner.run_experiment()
 
+#            shutil.copytree(dir + '/train', dir + '/test')
+#            os.mkdir(dir + '/test')
 if __name__ == "__main__":
     main()
