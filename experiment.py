5ebc3b514807ab4d6c7560d534bacd33be63ca12
pkorobov <korobov.i.am@gmail.com>
2020-06-04 05:50:57 +0300

    Update in fix_seed

--- experiment.py
+++ experiment.py
@@ -36,9 +36,11 @@ def create_wolp_agent(sess, env, eval_mode, k_ratio=0.1, summary_writer=None, **
 def fix_seed(seed):
     np.random.seed(seed)
     torch.manual_seed(seed)
+    random.seed(seed)
     if torch.cuda.is_available():
         torch.cuda.manual_seed_all(seed)
-    random.seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
 
 
 def run_agent(env, create_function, agent_name, base_dir,
